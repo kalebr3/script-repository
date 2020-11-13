1042c50e2c68831cf7dba72f8765f3cb0c2d05db
Kaleb Redman <kaleb@redman.us>
2020-11-13 14:29:07 -0500

    Added action to script

--- bash/install-imapsync.sh
+++ bash/install-imapsync.sh
@@ -44,7 +44,58 @@ echo -e "\nAre you sure you wish to install IMAP Sync and it's dependencies?\n"
 select yn in "Yes" "No"; do
 	case $yn in
 		Yes)
-            break;;
+			cd /tmp
+
+            echo -e "\n*********************************"
+			echo -e " INSTALLING DEPENDENCIES VIA APT "
+			echo -e "*********************************\n"
+			apt-get update
+			apt-get install -y \
+				libauthen-ntlm-perl \
+				libcgi-pm-perl \
+				libcrypt-openssl-rsa-perl \
+				libdata-uniqid-perl \
+				libencode-imaputf7-perl \
+				libfile-copy-recursive-perl \
+				libfile-tail-perl \
+				libio-socket-inet6-perl \
+				libio-socket-ssl-perl \
+				libio-tee-perl \
+				libhtml-parser-perl \
+				libjson-webtoken-perl \
+				libmail-imapclient-perl \
+				libparse-recdescent-perl \
+				libmodule-scandeps-perl \
+				libreadonly-perl \
+				libregexp-common-perl \
+				libsys-meminfo-perl \
+				libterm-readkey-perl \
+				libtest-mockobject-perl \
+				libtest-pod-perl \
+				libunicode-string-perl \
+				liburi-perl \
+				libwww-perl \
+				libtest-nowarnings-perl \
+				libtest-deep-perl \
+				libtest-warn-perl \
+				make \
+				cpanminus
+			
+			echo -e "\n************************************"
+			echo -e " DOWNLOADING APPLICATION: IMAP Sync "
+			echo -e "************************************\n"
+			wget https://raw.githubusercontent.com/imapsync/imapsync/master/imapsync
+
+			echo -e "\n***********************************"
+			echo -e " INSTALLING APPLICATION: IMAP Sync "
+			echo -e "***********************************\n"
+			chmod -x imapsync
+			mv imapsync /usr/bin/
+
+			echo -e "\n***********************************************"
+			echo -e " SUCCESSFULLY INSTALLED APPLICATION: IMAP Sync "
+			echo -e "***********************************************\n"
+			;;
         No)
 			break;;
 	esac
